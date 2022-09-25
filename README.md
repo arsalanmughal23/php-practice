1f5bcc8447411043f2dd523b4483cbacd57094b0
ArsalanMughal23 <arsalanmughal23@gmail.com>
2022-09-25 16:04:36 +0500

    Pring String, Words in Reverse && Array-Sorting

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # PHP Practice
 
 ## Palindrom Test
-* Swap 3 variable's values without using 4th variable
+* Swap 3 variable's values without using 4th variable.
+* Print String, Words in Reverse
+* Array Sorting
--- index.php
+++ index.php
@@ -19,3 +19,29 @@ echo "Number a =".$a.", b=".$b." and c=".$c."</br>";
 // echo "Number a =".$a." and b=".$b."\n";
 
 ?>
+
+
+<?php
+    echo '</br>';
+    // String Reverse
+    echo strrev("Hello World!");
+    echo '</br>';
+
+
+    $string = "Hello World hi i am";
+    $array = explode(" ", $string);
+    // Sentence Words Reverse
+    $rarray = array_reverse($array);
+
+    echo $newstring = implode(" ", $rarray);
+    echo '</br>';
+    echo '</br>';
+?>
+
+
+<?php
+    $age = array("Peter"=>"135", "Ben"=>"37", "Joe"=>"43");
+    ksort($age);
+
+    var_dump($age);
+?>
