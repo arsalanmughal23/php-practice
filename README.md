84c9b348c58fbb9d54297b38ce43f529b515f298
ArsalanMughal23 <arsalanmughal23@gmail.com>
2022-09-25 15:47:55 +0500

    Swap Values without using another variable

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # PHP Practice
+
+## Palindrom Test
+* Swap 3 variable's values without using 4th variable
--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
-echo 'php practice';
+
+$a = 15;
+$b = 27;
+$c = 30;
+ 
+// Swap Logic
+
+echo "\nThe number before swapping is:\n";
+echo "Number a =".$a.", b=".$b." and c=".$c."</br>";
+
+// $temp = $a;
+// $a = $b;
+// $b = $temp;
+list($a, $b, $c) = array($c, $a, $b);
+
+echo "\nThe number after swapping is: \n";
+echo "Number a =".$a.", b=".$b." and c=".$c."</br>";
+// echo "Number a =".$a." and b=".$b."\n";
+
 ?>
